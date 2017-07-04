82446b776800840e87bc9e84648bff150036e68f
jiajun.chpu <jiajun.chou@104.com.tw>
2017-07-04 18:17:05 +0800

    <test> Add 'continue' practice: Prime100COntinue
    Find 101-200 prime.
    Use continue outer to do the function equal to Goto
    
    trac #2566

---
+++ helloWorld/src/com/s104/tutor/Prime100Continue.java
@@ -0,0 +1,22 @@
+package com.s104.tutor;
+
+public class Prime100Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" *** 100-200 ªº½è¼Æ ***");
+		int n = 0;
+		outer: for(int i=101;i<200;i+=2) {
+			for(int j=2; j<i; j++) {
+				if( i%j ==0 ) continue outer;																									
+			}			
+			System.out.print(" " + i);
+			n++;
+			if(n < 10) continue;
+			System.out.println();
+			n=0;
+		}
+
+	}
+
+}
